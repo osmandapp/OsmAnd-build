1c46daa0b16466812f9d27195706a574e3038a4b
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-18 19:11:13 +0200

    Support eclipse

--- amd64-linux-gcc.sh
+++ amd64-linux-gcc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-if [ ! -n "$OSMAND_TARGET_PREFIX" ]
-then
+if [ ! -n "$OSMAND_TARGET_PREFIX" ]; then
     SNAME=`basename $0`
     OSMAND_TARGET_PREFIX=${SNAME%.*}
 fi
-OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=amd64-linux-gcc "$(dirname $0)/utils/makefile.sh" "$@"
+if [ -z "$OSMAND_BUILD_TOOL" ]; then
+	OSMAND_BUILD_TOOL=makefile
+fi
+OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=amd64-linux-gcc "$(dirname $0)/utils/$OSMAND_BUILD_TOOL.sh" "$@"
--- intel-darwin.sh
+++ intel-darwin.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-if [ ! -n "$OSMAND_TARGET_PREFIX" ]
-then
+if [ ! -n "$OSMAND_TARGET_PREFIX" ]; then
     SNAME=`basename $0`
     OSMAND_TARGET_PREFIX=${SNAME%.*}
 fi
