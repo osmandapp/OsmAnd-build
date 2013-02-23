8a667426c148f19d6753bca5cdb6e2cd43217638
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-23 20:54:16 +0200

    Normalize build

--- amd64-windows-vc11.cmd
+++ amd64-windows-vc11.cmd
@@ -1,8 +1,7 @@
 @echo off
 set ORIGINAL_DIR=%CD%
-"%~dp0\..\core\externals\qtbase-desktop\build.bat"
-rmdir /S /Q "%~dp0\..\baked\%~n0"
-mkdir "%~dp0\..\baked\%~n0"
-chdir "%~dp0\..\baked\%~n0"
+rmdir /S /Q "%~dp0..\baked\%~n0"
+mkdir "%~dp0..\baked\%~n0"
+chdir "%~dp0..\baked\%~n0"
 "%WINDOWS_CMAKE%\bin\cmake.exe" -G "Visual Studio 11 Win64" -DCMAKE_TARGET_OS:STRING=windows -DCMAKE_TARGET_CPU_ARCH:STRING=amd64 "%~dp0"
 chdir %ORIGINAL_DIR%
--- i686-linux-gcc.sh
+++ i686-linux-gcc.sh
@@ -6,5 +6,4 @@ fi
 if [ -z "$OSMAND_BUILD_TOOL" ]; then
 	OSMAND_BUILD_TOOL=makefile
 fi
-"../core/externals/qtbase-desktop/build.sh"
 OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=i686-linux-gcc "$(dirname $0)/utils/$OSMAND_BUILD_TOOL.sh" "$@"
