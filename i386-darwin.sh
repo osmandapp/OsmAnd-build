441675329a9130232237fa4fbb599054fdfd2087
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-05-10 12:18:22 +0300

    MacOSX build

---
+++ i386-darwin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -n "$OSMAND_TARGET_PREFIX" ]; then
+    SNAME=`basename $0`
+    OSMAND_TARGET_PREFIX=${SNAME%.*}
+fi
+JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=i386-darwin-clang "$(dirname $0)/utils/makefile.sh" "$@"
