b749e1938983d9a870e589fa031a752af353ce08
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-14 14:56:39 +0300

    Allow usage of tools other than make on Darwin

--- x86_64-darwin.sh
+++ x86_64-darwin.sh
@@ -3,4 +3,7 @@ if [ ! -n "$OSMAND_TARGET_PREFIX" ]; then
     SNAME=`basename $0`
     OSMAND_TARGET_PREFIX=${SNAME%.*}
 fi
-JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=x86_64-darwin-clang "$(dirname $0)/utils/makefile.sh" "$@"
+if [ -z "$OSMAND_BUILD_TOOL" ]; then
+	OSMAND_BUILD_TOOL=makefile
+fi
+JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=x86_64-darwin-clang "$(dirname $0)/utils/$OSMAND_BUILD_TOOL.sh" "$@"
