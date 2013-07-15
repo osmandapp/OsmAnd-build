161906f61ab6478c6a653cc7d09e72a0cc1b1b0b
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-15 09:02:41 +0300

    Add toolset specification

--- amd64-windows-vc11.cmd
+++ amd64-windows-vc11.cmd
@@ -1,7 +1,8 @@
 @echo off
 set ORIGINAL_DIR=%CD%
-rmdir /S /Q "%~dp0..\baked\%~n0"
-mkdir "%~dp0..\baked\%~n0"
-chdir "%~dp0..\baked\%~n0"
+set BAKED_DIR=%~dp0..\baked\%~n0.msvs2012
+rmdir /S /Q "%BAKED_DIR%"
+mkdir "%BAKED_DIR%"
+chdir "%BAKED_DIR%"
 "%WINDOWS_CMAKE%\bin\cmake.exe" -G "Visual Studio 11 Win64" -DCMAKE_TARGET_OS:STRING=windows -DCMAKE_TARGET_CPU_ARCH:STRING=amd64 "%~dp0"
 chdir %ORIGINAL_DIR%
