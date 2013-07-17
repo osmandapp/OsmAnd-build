197918f96b7ea5da4cfe7645ccb253d32b183d1e
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-17 17:47:32 +0300

    try 6

--- targets/device-ios-clang.cmake
+++ targets/device-ios-clang.cmake
@@ -2,19 +2,21 @@ set(CMAKE_TARGET_OS ios)
 set(CMAKE_TARGET_CPU_ARCH device)
 set(CMAKE_SHARED_LIBS_ALLOWED_ON_TARGET FALSE)
 set(CMAKE_STATIC_LIBS_ALLOWED_ON_TARGET TRUE)
-set(CMAKE_IOS_DEVELOPER_ROOT "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer")
-set(CMAKE_IOS_SDK_ROOT "${CMAKE_IOS_DEVELOPER_ROOT}/SDKs/iPhoneOS6.1.sdk")
-set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang)
-set(CMAKE_CXX_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++)
+execute_process(COMMAND xcodebuild -sdk iphoneos -find-executable clang OUTPUT_VARIABLE _CMAKE_C_COMPILER)
+set(CMAKE_C_COMPILER ${_CMAKE_C_COMPILER})
+execute_process(COMMAND xcodebuild -sdk iphoneos -find-executable clang++ OUTPUT_VARIABLE _CMAKE_CXX_COMPILER)
+set(CMAKE_CXX_COMPILER ${_CMAKE_CXX_COMPILER})
 set(CMAKE_COMPILER_FAMILY clang)
-set(CMAKE_C_COMPILER_FLAGS "-isysroot ${CMAKE_IOS_SDK_ROOT} -arch armv7 -arch armv7s -stdlib=libc++")
-set(CMAKE_CXX_COMPILER_FLAGS "-isysroot ${CMAKE_IOS_SDK_ROOT} -arch armv7 -arch armv7s -std=c++11 -stdlib=libc++")
+execute_process(COMMAND xcodebuild -sdk iphoneos -find-library system OUTPUT_VARIABLE _SDK_SYSTEMLIB)
+string(REPLACE "/usr/lib/libsystem.dylib" "" SDK_ROOT "${_SDK_SYSTEMLIB}")
+set(CMAKE_C_COMPILER_FLAGS "-isysroot ${SDK_ROOT} -arch armv7 -arch armv7s -stdlib=libc++")
+set(CMAKE_CXX_COMPILER_FLAGS "-isysroot ${SDK_ROOT} -arch armv7 -arch armv7s -std=c++11 -stdlib=libc++")
 
 set (CMAKE_FIND_FRAMEWORK FIRST)
 set (CMAKE_SYSTEM_FRAMEWORK_PATH
-	${CMAKE_IOS_SDK_ROOT}/System/Library/Frameworks
-	${CMAKE_IOS_SDK_ROOT}/System/Library/PrivateFrameworks
-	${CMAKE_IOS_SDK_ROOT}/Developer/Library/Frameworks
+	${SDK_ROOT}/System/Library/Frameworks
+	${SDK_ROOT}/System/Library/PrivateFrameworks
+	${SDK_ROOT}/Developer/Library/Frameworks
 )
 set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
