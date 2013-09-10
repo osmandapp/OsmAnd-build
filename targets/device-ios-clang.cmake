ac414ef7fa45782df379a73e2d526be1f137d251
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-10 09:53:29 +0300

    Assembler support for iOS

--- targets/device-ios-clang.cmake
+++ targets/device-ios-clang.cmake
@@ -2,16 +2,24 @@ set(CMAKE_TARGET_OS ios)
 set(CMAKE_TARGET_CPU_ARCH device)
 set(CMAKE_SHARED_LIBS_ALLOWED_ON_TARGET FALSE)
 set(CMAKE_STATIC_LIBS_ALLOWED_ON_TARGET TRUE)
+
+# C
 execute_process(COMMAND xcodebuild -sdk iphoneos -find-executable clang OUTPUT_VARIABLE _SDK_CLANG_PATH)
 string(STRIP "${_SDK_CLANG_PATH}" SDK_CLANG_PATH)
 set(CMAKE_C_COMPILER ${SDK_CLANG_PATH})
+
+# C++
 execute_process(COMMAND xcodebuild -sdk iphoneos -find-executable clang++ OUTPUT_VARIABLE _SDK_CLANGPP_PATH)
 string(STRIP "${_SDK_CLANGPP_PATH}" SDK_CLANGPP_PATH)
 set(CMAKE_CXX_COMPILER ${SDK_CLANGPP_PATH})
+
 set(CMAKE_COMPILER_FAMILY clang)
+
 execute_process(COMMAND xcodebuild -sdk iphoneos -find-library system OUTPUT_VARIABLE _SDK_SYSTEMLIB_PATH)
 string(STRIP "${_SDK_SYSTEMLIB_PATH}" SDK_SYSTEMLIB_PATH)
 string(REPLACE "/usr/lib/libsystem.dylib" "" SDK_ROOT "${SDK_SYSTEMLIB_PATH}")
+
+set(CMAKE_COMPILER_ASM_FLAGS "-isysroot ${SDK_ROOT} -arch armv7 -arch armv7s -stdlib=libc++ -miphoneos-version-min=6.0")
 set(CMAKE_COMPILER_C_FLAGS "-isysroot ${SDK_ROOT} -arch armv7 -arch armv7s -stdlib=libc++ -miphoneos-version-min=6.0")
 set(CMAKE_COMPILER_CXX_FLAGS "-isysroot ${SDK_ROOT} -arch armv7 -arch armv7s -std=c++11 -stdlib=libc++ -miphoneos-version-min=6.0")
 
