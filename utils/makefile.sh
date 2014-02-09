a4f26211d2a5a1346ebf6a7e4f45095744686ef7
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-09 09:17:40 +0200

    Adjust build path for Xcode

--- utils/makefile.sh
+++ utils/makefile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILD_TOOL_SUFFIX=makefile CMAKE_GENERATOR="Unix Makefiles" "$SCRIPT_LOCATION/common.sh" $*
+TARGET_BUILD_TOOL_SUFFIX=makefile CMAKE_GENERATOR="Unix Makefiles" "$SCRIPT_LOCATION/common.sh" $*
--- utils/xcode.sh
+++ utils/xcode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILD_TOOL_SUFFIX=xcode CMAKE_GENERATOR="Xcode" "$SCRIPT_LOCATION/common.sh" $*
+TARGET_BUILD_TOOL_SUFFIX=xcode CMAKE_GENERATOR="Xcode" "$SCRIPT_LOCATION/common.sh" $*
