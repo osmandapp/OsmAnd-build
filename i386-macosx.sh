82b094718efa2a5a556abd1bfd7b0070f5e460e7
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-05-24 12:51:29 +0300

    'Darwin' -> 'MacOSX'

--- i386-macosx.sh
+++ i386-macosx.sh
@@ -11,4 +11,4 @@ if [ -z "$OSMAND_BUILD_TOOL" ]; then
 	OSMAND_BUILD_TOOL=makefile
 fi
 
-JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=x86_64-darwin-clang "$SRCLOC/utils/$OSMAND_BUILD_TOOL.sh" "$@"
+JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=i386-macosx-clang "$SRCLOC/utils/$OSMAND_BUILD_TOOL.sh" "$@"
--- intel-macosx.sh
+++ intel-macosx.sh
@@ -11,4 +11,4 @@ if [ -z "$OSMAND_BUILD_TOOL" ]; then
 	OSMAND_BUILD_TOOL=makefile
 fi
 
-JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=intel-darwin-clang "$SRCLOC/utils/$OSMAND_BUILD_TOOL.sh" "$@"
+JAVA_HOME="`/usr/libexec/java_home`" OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=intel-macosx-clang "$SRCLOC/utils/$OSMAND_BUILD_TOOL.sh" "$@"
