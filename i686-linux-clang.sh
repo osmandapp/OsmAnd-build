fa25e0fdfabbc3d37ab70c6847c8295f60ee80e6
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-08-18 18:13:22 +0300

    Update build

---
+++ i686-linux-clang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$BASH_VERSION" ]; then
+	exec bash "$0" "$@"
+	exit $?
+fi
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "$OSMAND_BUILD_TOOL" ]; then
+	OSMAND_BUILD_TOOL=makefile
+fi
+
+OSMAND_TARGET_PREFIX="$OSMAND_TARGET_PREFIX" OSMAND_TARGET=i686-linux-clang "$SRCLOC/utils/$OSMAND_BUILD_TOOL.sh" "$@"
