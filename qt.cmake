fa87b71250339f49d4fd4ea41d10f0393474e69e
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-08-20 18:26:19 +0300

    Build system refactor

--- qt.cmake
+++ qt.cmake
@@ -21,6 +21,27 @@ else()
 		message(FATAL_ERROR "Failed to determine Qt flavour")
 	endif()
 
+	# Since Qt is also an upstream, configure it and build
+	set(QT_WAS_CONFIGURED -1)
+	prepare_upstream_ex("${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}" QT_WAS_CONFIGURED)
+	if (QT_WAS_CONFIGURED)
+		if (CMAKE_HOST_WIN32 AND NOT CYGWIN)
+			execute_process(
+				COMMAND cmd /C "build.bat ${CMAKE_TARGET_OS} ${CMAKE_COMPILER_FAMILY} ${CMAKE_TARGET_CPU_ARCH}"
+				WORKING_DIRECTORY "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}"
+				RESULT_VARIABLE EMBEDDED_QT_BUILD_RESULT)
+		else()
+			execute_process(
+				COMMAND "./build.sh ${CMAKE_TARGET_OS} ${CMAKE_COMPILER_FAMILY} ${CMAKE_TARGET_CPU_ARCH}"
+				WORKING_DIRECTORY "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}"
+				RESULT_VARIABLE EMBEDDED_QT_BUILD_RESULT)
+		endif()
+		
+		if (EMBEDDED_QT_BUILD_RESULT)
+			message(FATAL_ERROR "Failed to build embedded Qt for ${CMAKE_TARGET_OS}/${CMAKE_COMPILER_FAMILY}/${CMAKE_TARGET_CPU_ARCH}")
+		endif()
+	endif()
+
 	# If shared libs are allowed, they need shared Qt
 	if (CMAKE_SHARED_LIBS_ALLOWED_ON_TARGET)
 		if (EXISTS "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}.${CMAKE_COMPILER_FAMILY}-${CMAKE_TARGET_CPU_ARCH}.shared/lib/cmake")
@@ -40,8 +61,8 @@ else()
 	if (CMAKE_STATIC_LIBS_ALLOWED_ON_TARGET)
 		if (EXISTS "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}.${CMAKE_COMPILER_FAMILY}-${CMAKE_TARGET_CPU_ARCH}.static/lib/cmake")
 			set(OSMAND_EMBEDDED_QT_STATIC "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}.${CMAKE_COMPILER_FAMILY}-${CMAKE_TARGET_CPU_ARCH}.static")
-		elseif (EXISTS "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}${CMAKE_COMPILER_FAMILY}..static/lib/cmake")
-			set(OSMAND_EMBEDDED_QT_STATIC "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}${CMAKE_COMPILER_FAMILY}..static")
+		elseif (EXISTS "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}.${CMAKE_COMPILER_FAMILY}.static/lib/cmake")
+			set(OSMAND_EMBEDDED_QT_STATIC "${OSMAND_ROOT}/core/externals/qtbase-${OSMAND_QT_FLAVOUR}/upstream.patched.${CMAKE_TARGET_OS}.${CMAKE_COMPILER_FAMILY}.static")
 		endif()
 
 		if (NOT DEFINED OSMAND_EMBEDDED_QT_STATIC)
--- utils/common.cmake
+++ utils/common.cmake
@@ -1,27 +1,41 @@
-# prepare_upstream: Checks if upstream is present and is correctly configured
-macro(prepare_upstream)
+# prepare_upstream_ex: Checks if upstream is present and is correctly configured at specified path
+macro(prepare_upstream_ex UPSTREAM_BASE_PATH UPSTREAM_WAS_CONFIGURED)
 	# 'stamp' file is updated manually when anything is changed. So including this file will trigger
 	# cmake project regeneration. Regeneration itself will allow to check what should be performed.
-	include("${CMAKE_CURRENT_LIST_DIR}/stamp")
+	include("${UPSTREAM_BASE_PATH}/stamp")
 
-	# If stamps differ, upstream needs to be reconfigured
-	execute_process(
-		COMMAND ${CMAKE_COMMAND} -E compare_files "${CMAKE_CURRENT_LIST_DIR}/stamp" "${CMAKE_CURRENT_LIST_DIR}/.stamp"
-		RESULT_VARIABLE UPSTREAM_VERSION_STAMPS_DIFFER)
+	if (NOT EXISTS "${UPSTREAM_BASE_PATH}/upstream.patched")
+		set(UPSTREAM_VERSION_STAMPS_DIFFER 1)
+	else()
+		# If stamps differ, upstream needs to be reconfigured
+		execute_process(
+			COMMAND ${CMAKE_COMMAND} -E compare_files "${UPSTREAM_BASE_PATH}/stamp" "${UPSTREAM_BASE_PATH}/.stamp"
+			RESULT_VARIABLE UPSTREAM_VERSION_STAMPS_DIFFER)
+	endif()
+	
 	if (UPSTREAM_VERSION_STAMPS_DIFFER)
 		if (CMAKE_HOST_WIN32 AND NOT CYGWIN)
 			execute_process(
-				COMMAND cmd /C "bash --login ${CMAKE_CURRENT_LIST_DIR}/configure.sh"
-				WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+				COMMAND cmd /C "bash --login ${UPSTREAM_BASE_PATH}/configure.sh"
+				WORKING_DIRECTORY "${UPSTREAM_BASE_PATH}"
 				RESULT_VARIABLE UPSTREAM_CONFIGURE_FAILED)
 		else()
 			execute_process(
 				COMMAND "./configure.sh"
-				WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+				WORKING_DIRECTORY "${UPSTREAM_BASE_PATH}"
 				RESULT_VARIABLE UPSTREAM_CONFIGURE_FAILED)
 		endif()
 		if (UPSTREAM_CONFIGURE_FAILED)
 			message(FATAL_ERROR "Failed to configure upstream: ${UPSTREAM_CONFIGURE_FAILED}")
 		endif()
+		set(${UPSTREAM_WAS_CONFIGURED} 1)
+	else()
+		set(${UPSTREAM_WAS_CONFIGURED} 0)
 	endif()
+endmacro()
+
+# prepare_upstream: Checks if upstream is present and is correctly configured at current CMakeList directory
+macro(prepare_upstream)
+	set(UPSTREAM_WAS_CONFIGURED -1)
+	prepare_upstream_ex(${CMAKE_CURRENT_LIST_DIR} UPSTREAM_WAS_CONFIGURED)
 endmacro()
