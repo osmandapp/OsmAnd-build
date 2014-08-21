6b9920e71fc65fdf55628729de326a41f429e649
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-08-21 08:43:25 +0300

    Refactor

--- utils/generate.sh
+++ utils/generate.sh
@@ -8,14 +8,14 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Verify environment
-if [[ -z "$OSMAND_TARGET" ]]; then
-	echo "Building for target '${OSMAND_TARGET}'"
+if [[ -n "$OSMAND_TARGET" ]]; then
+	echo "Baking project files target '${OSMAND_TARGET}'"
 	OSMAND_TARGET_SPECIFICATION="-DOSMAND_TARGET=${OSMAND_TARGET}"
 	if [[ -f "$SRCLOC/../target/${OSMAND_TARGET}.sh" ]]; then
 		source "$SRCLOC/../target/${OSMAND_TARGET}.sh"
 	fi
-elif [[ -z "$OSMAND_CROSSPLATFORM_TARGET" ]]; then
-	echo "Building for cross-platform target '${OSMAND_TARGET}'"
+elif [[ -n "$OSMAND_CROSSPLATFORM_TARGET" ]]; then
+	echo "Baking project files for cross-platform target '${OSMAND_TARGET}'"
 	OSMAND_TARGET_SPECIFICATION="-DCMAKE_TOOLCHAIN_FILE=targets/${OSMAND_CROSSPLATFORM_TARGET}.cmake"
 else
 	echo "OSMAND_TARGET and OSMAND_CROSSPLATFORM_TARGET is not set - one of them needs to be set"
@@ -41,7 +41,7 @@ then
 fi
 if [ -n "$CMAKE_BUILD_TYPE" ]
 then
-	echo "Building in $CMAKE_BUILD_TYPE mode"
+	echo "Target build mode: $CMAKE_BUILD_TYPE"
 	CMAKE_BUILD_TYPE="-DCMAKE_BUILD_TYPE:STRING=$CMAKE_BUILD_TYPE"
 	BUILD_TYPE_SUFFIX="-$BUILD_TYPE_SUFFIX"
 fi
@@ -50,7 +50,7 @@ fi
 OSMAND_CPU_SPECIFIC_DEFINE=""
 CPU_SPECIFIC_SUFFIX=""
 if [ -n "$OSMAND_SPECIFIC_CPU_NAME" ]; then
-	echo "Building for CPU : $OSMAND_SPECIFIC_CPU_NAME"
+	echo "Specific CPU: $OSMAND_SPECIFIC_CPU_NAME"
 	OSMAND_CPU_SPECIFIC_DEFINE="-DCMAKE_SPECIFIC_CPU_NAME:STRING=$OSMAND_SPECIFIC_CPU_NAME"
 	CPU_SPECIFIC_SUFFIX="-$OSMAND_SPECIFIC_CPU_NAME"
 fi
@@ -64,10 +64,12 @@ fi
 # Check for specific compiler selected
 SPECIFIC_CC_COMPILER=""
 if [[ -n "$OSMAND_CC" ]]; then
+	echo "Using specific C compiler: ${OSMAND_CC}"
 	SPECIFIC_CC_COMPILER="CC=${OSMAND_CC}"
 fi
 SPECIFIC_CXX_COMPILER=""
 if [[ -n "$OSMAND_CXX" ]]; then
+	echo "Using specific C++ compiler: ${OSMAND_CXX}"
 	SPECIFIC_CXX_COMPILER="CXX=${OSMAND_CXX}"
 fi
 
