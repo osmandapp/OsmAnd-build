0623a07927f0c1a624415bfaf2471abf736e7530
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-08-29 09:40:17 +0300

    Fix

---
+++ extras/android.cmake
@@ -0,0 +1,10 @@
+# Extra projects for android target
+
+project(android_cpufeatures)
+add_library(android_cpufeatures STATIC
+	"${ANDROID_NDK}/sources/android/cpu-features.c"
+)
+target_include_directories(android_cpufeatures
+	PUBLIC
+		"${ANDROID_NDK}/sources/android/cpufeatures"
+)
