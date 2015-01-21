fc470412be2c830dd12d5937feb9771f2e5a990d
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-01-21 16:06:38 +0200

    Fix

--- targets/i686-windows-gcc.cmake
+++ targets/i686-windows-gcc.cmake
@@ -10,7 +10,7 @@ set(CMAKE_COMPILER_FAMILY gcc)
 set(CMAKE_ASM_FLAGS "-m32")
 set(CMAKE_C_FLAGS "-m32")
 set(CMAKE_CXX_FLAGS "-m32")
-set(CMAKE_SHARED_LINKER_FLAGS "-mdll -static-libgcc -static-libstdc++")
+set(CMAKE_SHARED_LINKER_FLAGS "-static-libgcc -static-libstdc++")
 set(CMAKE_MODULE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
 set(CMAKE_EXE_LINKER_FLAGS "-mconsole -static-libgcc -static-libstdc++")
 
